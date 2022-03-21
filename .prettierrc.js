ea538907af13a4c4fc80e37f79c34aa831dd2b61
kvernev <kverenev@gmail.com>
2022-03-21 10:14:44 +0300

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+};
